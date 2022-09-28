7ee28f641f99720767abdcd86035e958e5e8b438
Daniel Ortiz <daniel_ortizpary@hotmail.com>
2022-09-28 10:41:57 -0500

    modifications

--- model/Passage.py
+++ model/Passage.py
@@ -1,11 +1,11 @@
 from Route import Route
 
 class Passage(object):
-	def __init__(self, passage_id:int, passenger_name:str, route:Route, price:float):
+	def __init__(self, passage_id:int, passenger_name:str, tax:float, gross_price:float):
 		
 		self.passage_id: int = passage_id
 		self.passenger_name: str = passenger_name
-		self.route: Route = route 
-		self.price: float = price
+		self.tax: float = tax 
+		self.gross_price: float = gross_price
 
 		pass
--- model/Route.py
+++ model/Route.py
@@ -9,16 +9,19 @@ class Route(object):
         economy_class:float,
         premiun_class:float
     ):
-        self.code = CITY_TO_CODE[_from]+" - "+CITY_TO_CODE[_to]
-        self.name = _from+" - "+_to
-        self.base_price = base_price
-        self.economy_class = economy_class
-        self.premiun_class = premiun_class
+        self.code : str = CITY_TO_CODE[_from]+" - "+CITY_TO_CODE[_to]
+        self.name : str = _from+" - "+_to
+        self._from : str = _from
+        self._to : str = _to
+        self.base_price : float = base_price
+        self.economy_class : float = economy_class
+        self.premiun_class : float = premiun_class
 
     def __repr__(self) -> str:
         """
         Special method to represent the object of a class as a string
         """
+
         return self.code
     
     def generate_price_economy_class(self) -> float:
@@ -34,3 +37,17 @@ class Route(object):
         """
  
         return (self.base_price+self.premiun_class)*(1.0+(float(IGV_PERCENT)/100.0))
+    
+    def get_code_from(self)->str:
+        """
+        get city code from route _from
+        """
+
+        return CITY_TO_CODE[self._from]
+
+    def get_code_to(self)->str:
+        """
+        get city code from route _to
+        """
+
+        return CITY_TO_CODE[self._to]
