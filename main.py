516cb76c3a8d347bcf4b80bf475d94e077334b31
unknown <bdonecat2001@gmail.com>
2022-09-29 16:23:51 -0500

    question nine in progress

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import controller
 
 
 def main():
-    print(str(controller.question_one()))
-    print(str(controller.question_two()))
-    print(str(controller.question_three()))
-    print(str(controller.question_four()))
-    print(str(controller.question_five()))
-    print(str(controller.question_six()))
-    controller.question10()
+    #print(str(controller.question_one()))
+    #print(str(controller.question_two()))
+    #print(str(controller.question_three()))
+    #print(str(controller.question_four()))
+    #print(str(controller.question_five()))
+    #print(str(controller.question_six()))
+    controller.question_nine()
+    #controller.question10()
 
 
 if __name__ == "__main__":
--- model/Timeline.py
+++ model/Timeline.py
@@ -128,3 +128,51 @@ class Timeline(object):
 
         print(
             f"El avión con más pasajeros es: {airplane_code}. Con un total de {greater_number_passengers}.")
+    # This method print the flight with greatest number passengers
+
+    def get_greatest_numbers_passengers(self) -> None:
+        # Airplane code with greatest number passengers
+        airplane_code: str = ''
+        # Greatest numbers of passengers among the flights
+        greater_number_passengers: int = 0
+
+        # Iterate all the flights of the day
+        for i in self.flights:
+            # print(len(i.passages))
+            # Compare and get greatest number passengers
+            if (greater_number_passengers < len(i.passages)):
+                airplane_code = i.airplane.code
+                greater_number_passengers = len(i.passages)
+
+        print(
+            f"El avión con más pasajeros es: {airplane_code}. Con un total de {greater_number_passengers}.")
+
+    def get_the_first_three_flights_with_the_highest_sales(self) -> str:
+
+        venta_por_vuelo = []
+
+        for a in self.flights:
+            ventas_por_pasaje = []
+            suma_ventas_por_pasaje = 0
+            for e in a.passages:
+                ventas_por_pasaje.append(e.get_net_price())
+
+            suma_ventas_por_pasaje = sum(ventas_por_pasaje)
+
+            venta_por_vuelo.append(suma_ventas_por_pasaje)
+
+        print(venta_por_vuelo, "\n")
+
+        tres_primeros = []
+
+        for i in venta_por_vuelo:
+            if len(tres_primeros) == 3:
+                break
+            if len(tres_primeros) == 0:
+                tres_primeros.append(venta_por_vuelo[0] if venta_por_vuelo[0] > venta_por_vuelo[1] else venta_por_vuelo[1])
+            else:
+                tres_primeros = sorted(tres_primeros, reverse=False)
+                if i > tres_primeros[0]:
+                    tres_primeros.append(i)
+        print(tres_primeros)
+
